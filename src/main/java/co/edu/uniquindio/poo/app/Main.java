10b0456d715fe671bf4a7109657566305923d117
Jose15z <josem.rodriguezo@uqvirtual.edu.co>
2025-04-08 16:53:19 -0500

    Eyy

--- src/main/java/co/edu/uniquindio/poo/app/Main.java
+++ src/main/java/co/edu/uniquindio/poo/app/Main.java
@@ -1,14 +1,16 @@
 package co.edu.uniquindio.poo.app;
 
+import co.edu.uniquindio.poo.model.Flor;
 import co.edu.uniquindio.poo.model.MiniMapa;
+import co.edu.uniquindio.poo.model.TipoFlor;
+import co.edu.uniquindio.poo.model.TipoFlorFactory;
 
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         MiniMapa mapa = new MiniMapa(8,8);
-        Scanner scanner = new Scanner(System.in);
-
         //es de 0 a 7 el mapa
         mapa.plantarFlor(2,3, "Margarita");
         mapa.plantarFlor(4, 5, "Tulipan");
@@ -16,20 +18,53 @@ public class Main {
         mapa.plantarFlor(6, 3, "Rosa");
         mapa.plantarFlor(7, 7, "Cerezo");
 
+        System.out.println("===================================");
         System.out.println("Bienvenido al MiniMapa con Flores");
+        System.out.println("===================================");
+
         boolean ejecutando = true;
+
         mapa.mostrarMapa();
+
         while(ejecutando) {
+        System.out.println("\n------- MENU PRINCIPAL -------");
         System.out.print("\n1.Plantar flor: ");
         System.out.print("\n2.Mostrar mapa: ");
         System.out.print("\n3.Salir ");
         System.out.print("\nEscribe tu opcion: ");
 
-        String opcion = scanner.nextLine();
+        String opcion = sc.nextLine();
 
-            //hacer el switch para el menu
-            //los comentarios
-            // el metodo que maneje cuando el tipo de flor sea null (ver TipoFlor!!)
+            switch (opcion){
+                case "1":
+                    try {
+                        System.out.print("En que posicion en x quiere plantar flor: ");
+                        int x = sc.nextInt();
+                        System.out.print("En que posicion en y quiere plantar flor: ");
+                        int y = sc.nextInt();
+                        sc.nextLine();
+
+                        TipoFlorFactory tipoFlorFactory = new TipoFlorFactory();
+                        String tipoFlor = tipoFlorFactory.ingresarTipo();
+
+                        mapa.plantarFlor(x, y, tipoFlor);
+                        System.out.println("Flor plantada con exito.");
+                    }
+                    catch(Exception e){
+                        System.out.println("Entrada inválida. Intenta de nuevo.");
+                    }
+                    break;
+                case "2":
+                     mapa.mostrarMapa();
+                    break;
+                case "3":
+                    ejecutando = false;
+                    break;
+
+
+            }
         }
+
     }
+
 }
--- src/main/java/co/edu/uniquindio/poo/model/TipoFlorFactory.java
+++ src/main/java/co/edu/uniquindio/poo/model/TipoFlorFactory.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.poo.model;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class TipoFlorFactory {
     private static final HashMap<String, TipoFlor> listaTiposFlores = new HashMap<>();
@@ -24,6 +25,32 @@ public class TipoFlorFactory {
         }
 
         return listaTiposFlores.get(tipo);
+    }
+
+    public String ingresarTipo() {
+        System.out.print("Que tipo de flor desea plantar(Margarita, Tulipan, Rosa, Cerezo): ");
+        System.out.print("\n1.Margarita: 🌼");
+        System.out.print("\n2.Tulipan 🌷 ");
+        System.out.print("\n3.Rosa 🌹");
+        System.out.println("\n4.Cerezo 🌸");
+        Scanner sc = new Scanner(System.in);
+        String tipo = "";
+        String opcion = sc.nextLine();
+        switch (opcion) {
+            case "1":
+                tipo = "Margarita";
+                break;
+            case "2":
+                tipo = "Tulipan";
+                break;
+            case "3":
+                tipo = "Rosa";
+                break;
+            case "4":
+                tipo = "Cerezo";
+                break;
+        }
+        return tipo;
     }
 
 }
