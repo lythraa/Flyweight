4fadb856c0f32585d115150b7f0aa4c6d58966bf
NicolasOrozco <nicolas.orozcoo@uqvirtual.edu.co>
2025-04-08 19:13:51 -0500

    Final no comments

--- src/main/java/co/edu/uniquindio/poo/model/TipoFlor.java
+++ src/main/java/co/edu/uniquindio/poo/model/TipoFlor.java
@@ -1,16 +1,12 @@
 package co.edu.uniquindio.poo.model;
 //INTRINSECA = NO ES MODIFICABLE, SOLO SE CREA UNA VEZ
 public class TipoFlor {
-    private String nombre;
-    private String simbolo;
+    private final String nombre;
+    private final String simbolo;
 
     public TipoFlor(String nombre, String simbolo) {
         this.nombre = nombre;
         this.simbolo = simbolo;
-    }
-
-    public String getNombre() {
-        return nombre;
     }
 
     public String getSimbolo() {
